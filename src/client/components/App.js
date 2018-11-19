c951b423824b3b04b31d9dc93a684750986076c4
Artem <artem.ft@gmail.com>
2018-11-19 20:51:59 +0300

    tmp

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -39,30 +39,10 @@ export default class App extends React.PureComponent {
       streams
     } = this.props
 
-    let first = _.first(peers)
-    // let first = constants.ME
-    let firstPeerVideo = ''
-    if (first !== undefined) {
-      firstPeerVideo = <div className="col-md-6"><Video
-        active={first === active}
-        key={first}
-        onClick={toggleActive}
-        stream={streams[first]}
-        userId={first}
-      /></div>
-    }
-
-    let tailPeers = _.rest(peers)
-    let n = 2
-    let lists = _.groupBy(tailPeers, function (element, index) {
-      return Math.floor(index / n)
-    })
-    lists = _.toArray(lists)
-
     return (<div className="app">
-      {/* <Alerts alerts={alerts} dismiss={dismissAlert} /> */}
-      {/* <Notifications notifications={notifications} /> */}
-      {/* <Input notify={notify} sendMessage={sendMessage} /> */}
+       <Alerts alerts={alerts} dismiss={dismissAlert} />
+       <Notifications notifications={notifications} />
+       <Input notify={notify} sendMessage={sendMessage} />
       <div className="videos container">
         <div className="row justify-content-md-center">
           <div className="col-md-6">
@@ -73,24 +53,21 @@ export default class App extends React.PureComponent {
               userId={constants.ME}
             />
           </div>
-          {firstPeerVideo}
 
         </div>
 
-        {_.map(lists, (group) => (
-          <div className="row justify-content-md-center">
-            {_.map(group, (userId) =>
-              <div className="col-md-6">
-                <Video
-                  active={active === userId}
-                  onClick={toggleActive}
-                  stream={streams[userId]}
-                  userId={userId}
-                />
-              </div>
-            )}
-          </div>
-        ))}
+        <div className="row justify-content-md-center">
+          {_.map(peers, (_, userId) => (
+            <div className="col">
+              <Video
+                active={active === userId}
+                onClick={toggleActive}
+                stream={streams[userId]}
+                userId={userId}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>)
   }
